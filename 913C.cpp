63c4ceac4b105aa399f6788b2a436c921c99cba2
apurvparekh30 <apurvparekh30@gmail.com>
2020-04-18 23:55:40 -0400

    913C.cpp

--- 913C.cpp
+++ 913C.cpp
@@ -2,48 +2,44 @@
 
 using namespace std;
 
-int n;
-int L;
+int n,L;
+int64_t cost[31];
+int64_t right_[31];
 
-long long int cost[30];
-
-long long int rec(int ltr) {
-  long long int min_cost = cost[0];
-  int min_cost_index = 0;
-  int min_multiplier = ltr;
-  for(int i=1;i<n;i++) {
-    if(min_cost > cost[i]) {
-      min_cost = cost[i];
-      min_cost_index = i;
-      continue;
-    }
-    int q1 = ltr / (1 << min_cost_index);
-    if(ltr % (1 << min_cost_index) != 0) {
-      q1 = q1 + 1;
-    }
-    long long int cost1 = min_cost << q1;
-    int q2 = ltr / (1 << i);
-    if(ltr % (1<<i) != 0) {
-      q2 = q2 + 1;
-    }
-    long long int cost2 = cost[i] << q2;
-    if(cost1 > cost2) {
-      min_cost = cost[i];
-      min_cost_index = i;
-      min_multiplier = q2;
-    }
+int64_t rec(int L) {
+  //cout << L << "\n";
+  if(L <= 0) return 0;
+  int idx = static_cast<int>(log2(L));
+  //cout << idx << "\n";
+  int64_t ans = 0;
+  if(idx > n-1) {
+    int q = L / (1 << (n-1));
+    ans = (q * cost[n-1]) + rec(L - (q * (1 << (n-1))));
   }
-  cout << min_cost << " " << min_cost_index << " " << min_multiplier << "\n";
-  long long int ret = min_cost * min_multiplier; 
-  return ret;
+  else if(idx == right_[idx]){
+    ans = cost[idx] + rec(L - (1<<idx));
+  } else {
+    ans = cost[right_[idx]];
+    ans = min(ans, cost[idx] + rec(L - (1<<idx)));
+  }
+  return ans;
 }
 
 int main() {
   cin >> n >> L;
   for(int i=0;i<n;i++) {
     cin >> cost[i];
+    if(i > 0 && (cost[i-1]*2) < cost[i]) {
+      cost[i] = cost[i-1] * 2;
+    }
   }
-  long long int ans = rec(L);
+  right_[n-1] = n-1;
+  for(int i=n-2;i>=0;i--) {
+    if(cost[right_[i+1]] < cost[i+1])  
+      right_[i] = right_[i+1];
+    else
+      right_[i] = i+1;
+  }
+  int64_t ans = rec(L);
   cout << ans << "\n";
-  return 0;
 }
